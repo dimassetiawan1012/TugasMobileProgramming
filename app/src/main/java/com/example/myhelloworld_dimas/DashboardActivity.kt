a9c1b85adf84aebc877a1e70f1c98d8e64cca579
dimassetiawan1012 <71221907+dimassetiawan1012@users.noreply.github.com>
2021-04-01 11:11:21 +0700

    Commit UTS Perkiraan Cuaca Dimas

--- app/src/main/java/com/example/myhelloworld_dimas/DashboardActivity.kt
+++ app/src/main/java/com/example/myhelloworld_dimas/DashboardActivity.kt
@@ -4,25 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_dashboard.*
 import okhttp3.*
 import java.io.IOException
 
 class DashboardActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        //run("https://api.openweathermap.org/data/2.5/forecast?q=purwakarta,ID&appid=2ce659b9c25fc6fe3a07de4ca71d1dac")
+//        run("https://api.openweathermap.org/data/2.5/forecast?q=purwakarta,ID&appid=2ce659b9c25fc6fe3a07de4ca71d1dac")
 
         NetworkConfig().getService().getWeathers("purwakarta,ID", "2ce659b9c25fc6fe3a07de4ca71d1dac").enqueue(object : retrofit2.Callback<ResultWeather> {
 
             override fun onResponse(call: retrofit2.Call<ResultWeather>, response: retrofit2.Response<ResultWeather>) {
-                //var item = response.body()
+                var item = response.body()
                 Log.d("response", response.body().toString())
 
-                //txtKota.setText(item.city.name)
+                txtKota.text = item?.city?.name
+                txtTanggal.text = item?.list?.get(0)?.dt?.let { Util.getDayName(it.toLong()) }
+                text_temp.text = item?.list?.get(0)?.main?.temp?.let { Util.setFormatTemperature(it) }
+                text_temp_min_max.text = item?.list?.get(0)?.main?.tempMin?.let { Util.setFormatTemperature(it) } + " - " + item?.list?.get(0)?.main?.tempMax?.let { Util.setFormatTemperature(it) }
+                text_desc.text = item?.list?.get(0)?.weather?.get(0)?.description.toString()
+                item?.list?.get(0)?.weather?.get(0)?.id?.let { Util.getArtResourceForWeatherCondition(it) }?.let { image_desc.setImageResource(it) }
+
+                var list = item?.list
+                var itemAdp = ItemAdapter(list as List<ListItem>)
+
+                recycler_view_container.apply {
+                    layoutManager = LinearLayoutManager(this@DashboardActivity)
+                    adapter = itemAdp
+                }
             }
 
             override fun onFailure(call: retrofit2.Call<ResultWeather>, t: Throwable) {
@@ -31,7 +45,6 @@ class DashboardActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, t.message.toString(), Toast.LENGTH_LONG).show()
             }
         })
-
     }
 
     fun run(url: String){
@@ -42,7 +55,7 @@ class DashboardActivity : AppCompatActivity() {
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {}
-            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+            override fun onResponse(call: Call, response: Response) = println(response.body?.string())
         })
     }
 }
